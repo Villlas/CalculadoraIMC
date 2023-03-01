a0cd705762d3ea98c527b650cc986e79579e1039
OGabrielVillarinho <gabrielsoaresvillarinho1@gmail.com>
2023-02-28 21:32:49 -0300

    First Commit

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const calcular = document.getElementById("calcular");
+
+function main() {
+  const altura = +document.getElementById("altura").value;
+  const peso = +document.getElementById("peso").value;
+  const resultado = document.getElementById("resultado");
+  if (altura !== "" && peso !== "") {
+    const valorIMC = calcularIMC(altura, peso);
+
+    let classificacao = "";
+    classificacao = classificarIMC(valorIMC);
+
+    resultado.textContent = "IMC: " + valorIMC + " | " + classificacao;
+  } else {
+    resultado.textContent = "Preencha todos os campos!!!";
+  }
+}
+
+function calcularIMC(altura, peso) {
+  let valor;
+  valor = (peso / altura ** 2).toFixed(1);
+  return valor;
+}
+
+function classificarIMC(imc) {
+  let grau = "";
+  if (imc < 18.5) grau = "Abaixo do peso.";
+  else if (imc < 25) grau = "Peso Ideal";
+  else if (imc < 30) grau = "Sobrepeso";
+  else if (imc < 35) grau = "Obesidade grau I";
+  else if (imc < 40) grau = "Obesidade grau II";
+  else grau = "Obesidade mórbida";
+  return grau;
+}
+
+calcular.addEventListener("click", main);
